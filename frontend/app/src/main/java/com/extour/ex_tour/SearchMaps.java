3163ead74e137ae6a6439742403f86c451f96864
Teresa Chaves <up201306842@fe.up.pt>
2017-04-19 23:45:04 +0100

    frontend para o prototipo funcional

---
+++ frontend/app/src/main/java/com/extour/ex_tour/SearchMaps.java
@@ -0,0 +1,47 @@
+package com.extour.ex_tour;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SearchMaps extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng porto = new LatLng(41.149968, -8.610243);
+        mMap.addMarker(new MarkerOptions().position(porto).title("Marker in Porto"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(porto));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 10.0f));
+    }
+}
